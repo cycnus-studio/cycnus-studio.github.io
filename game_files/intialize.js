791a19bbe188f6ccb92592f212bc9feacf6042bb
cycnus-studio <50322929+cycnus-studio@users.noreply.github.com>
2019-06-04 18:50:43 -0400

    Add files via upload

---
+++ game_files/intialize.js
@@ -0,0 +1,17 @@
+
+// Set dimensions
+
+const HEIGHT  = 800;
+const WIDTH   = 1200;
+const PADDING = 100;
+
+const MID_HEIGHT = HEIGHT / 2; 
+const MID_WIDTH  = WIDTH / 2;
+
+const SPRITE_SIZE = 10;
+
+const MIN_OBSTACLES = 2;
+const MAX_OBSTACLES = 6;
+
+const MIN_BLOCK_SIZE = 10;
+const MAX_BLOCK_SIZE = 40;
