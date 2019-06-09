3b68c42d15d7ffe4583cc9fbf3d4862c1d872482
Jamie1024 <49384456+Jamie1024@users.noreply.github.com>
2019-06-08 21:17:22 -0400

    Update button_test.js

--- Project/src/button_test.js
+++ Project/src/button_test.js
@@ -2,56 +2,125 @@ PIXI.loader
 	.add([
 		"https://cycnus-studio.github.io/Project/img/freezeButton.png",
 		"https://cycnus-studio.github.io/Project/img/freezeButtonDown.png",
-		"https://cycnus-studio.github.io/Project/img/freezeButtonOver.png"
+		"https://cycnus-studio.github.io/Project/img/freezeButtonOver.png",
+		"https://cycnus-studio.github.io/Project/img/speedButton.png",
+		"https://cycnus-studio.github.io/Project/img/speedButtonDown.png",
+		"https://cycnus-studio.github.io/Project/img/speedButtonOver.png",
+		"https://cycnus-studio.github.io/Project/img/helperButton.png",
+		"https://cycnus-studio.github.io/Project/img/helperButtonDown.png",
+		"https://cycnus-studio.github.io/Project/img/helperButtonOver.png", 
+		"https://cycnus-studio.github.io/Project/img/laserButton.png",
+		"https://cycnus-studio.github.io/Project/img/laserButtonDown.png",
+		"https://cycnus-studio.github.io/Project/img/laserButtonOver.png"
 	])
 	.load(setup1);
+	
+var freezeTexture, freezeTextureDown, freezeTextureOver;
+var speedTexture, speedTextureDown, speedTextureOver; 
+var helperTexture, helperTextureDown, helperTextureOver; 
+var laserTexture, laserTextureDown, laserTextureOver; 
     
+var freezeArr, speedArr, helperArr, laserArr;
+
+powerArr = [freezeTexture, speedTexture, helperTexture, laserTexture];
+powerDownArr = [freezeTextureDown, speedTextureDown, helperTextureDown, laserTextureDown];
+powerOverArr = [freezeTextureOver, speedTextureOver, helperTextureOver, laserTextureOver];
+powerStringArr = ["freeze", "speed", "helper", "laser"]; 
+xArr = [220, 320, 420, 520];
+
+var buttonTexture, buttonTextureDown, buttonTextureOver, xValue, buttonString;
+
 function setup1() {
 	
-	var freezeTexture = PIXI.Texture.from("https://cycnus-studio.github.io/Project/img/freezeButton.png");
-	var freezeTextureDown = PIXI.Texture.from("https://cycnus-studio.github.io/Project/img/freezeButtonDown.png");
-	var freezeTextureOver = PIXI.Texture.from("https://cycnus-studio.github.io/Project/img/freezeButtonOver.png");
-
-	var freezeButton = new PIXI.Sprite(freezeTexture);
-
-  	freezeButton.interactive = true; 
-	freezeButton.buttonMode = true;
+	for (x = 0; x < powerArr.length; x++) {
+		
+		buttonTexture = powerArr[x];
+		buttonTextureDown = powerDownArr[x];
+		buttonTextureOver = powerOverArr[x];
+		buttonString = powerStringArr[x];
+		xValue = xArr[x];
 	
-	freezeButton.width = 80;
-	freezeButton.height = 80;
-	freezeButton.x = 220;
-	freezeButton.y = 710;
-	freezeButton.scale.set = 0.5;
-	  
-	app.stage.addChild(freezeButton);
-  
-	freezeButton //,speedButton, 
-        // set the mousedown and touchstart callback...
-        .on('mousedown', onButtonDown)
-        .on('touchstart', onButtonDown)
+		buttonTexture = PIXI.Texture.from("https://cycnus-studio.github.io/Project/img/" + buttonString + "Button.png");
+		buttonTextureDown = PIXI.Texture.from("https://cycnus-studio.github.io/Project/img/" + buttonString + "ButtonDown.png");
+		buttonTextureOver = PIXI.Texture.from("https://cycnus-studio.github.io/Project/img/" + buttonString + "ButtonOver.png");
 
-        // set the mouseup and touchend callback...
-        .on('mouseup', onButtonUp)
-        .on('touchend', onButtonUp)
-        .on('mouseupoutside', onButtonUp)
-        .on('touchendoutside', onButtonUp)
+		console.log(x, ": ", buttonTexture);
+		
+		var powerButton = new PIXI.Sprite(buttonTexture);
 
-        // set the mouseover callback...
-        .on('mouseover', onButtonOver)
+		powerButton.interactive = true; 
+		powerButton.buttonMode = true;
+		
+		powerButton.width = 80;
+		powerButton.height = 80;
+		powerButton.x = xValue;
+		powerButton.y = 710;
+		powerButton.scale.set = 0.5;
+		
+		app.stage.addChild(powerButton);
+	
+		function onButtonDown() {
+			this.isdown = true;
+			console.log("buttondown");
+			this.texture = buttonTextureDown;
+			this.alpha = 1;
+		}
 
-        // set the mouseout callback...
-        .on('mouseout', onButtonOut)
+		function onButtonUp() {
+			this.isdown = false;
+			console.log("buttonup");
+			if (this.isOver) {
+				this.texture = buttonTextureOver;
+			} else {
+				this.texture = buttonTexture;
+			}
+		}
+
+		function onButtonOver() {
+			console.log("buttonover");
+			this.isOver = true;
+			if (this.isdown) {
+				return;
+			}
+			this.texture = buttonTextureOver;
+		}
+
+		function onButtonOut() {
+			console.log("buttonout");
+			this.isOver = false;
+			if (this.isdown) {
+				return;
+			}
+			this.texture = buttonTexture;
+		}
+		
+		powerButton 
+			.on('mousedown', onButtonDown()) //can you pass variables into these functions
+			.on('touchstart', onButtonDown)
+
+			.on('mouseup', onButtonUp)
+			.on('touchend', onButtonUp)
+			.on('mouseupoutside', onButtonUp)
+			.on('touchendoutside', onButtonUp)
+
+			.on('mouseover', onButtonOver)
+
+			.on('mouseout', onButtonOut)
+			
+	}
   
 }
-
+/*
 function onButtonDown() {
     this.isdown = true;
+	console.log("buttondown");
     this.texture = freezeTextureDown;
     this.alpha = 1;
 }
 
 function onButtonUp() {
     this.isdown = false;
+	console.log("buttonup");
     if (this.isOver) {
         this.texture = freezeTextureOver;
     } else {
@@ -60,6 +129,7 @@ function onButtonUp() {
 }
 
 function onButtonOver() {
+	console.log("buttonover");
     this.isOver = true;
     if (this.isdown) {
         return;
@@ -68,9 +138,10 @@ function onButtonOver() {
 }
 
 function onButtonOut() {
+	console.log("buttonout");
     this.isOver = false;
     if (this.isdown) {
         return;
     }
     this.texture = freezeTexture;
-}
+}*/
