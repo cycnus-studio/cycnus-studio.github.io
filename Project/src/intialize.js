d2e18b5dd7b0445639cc35a55dfa712f45d7f639
Zeyu Chen <ZYCM03@gmail.com>
2019-06-09 18:51:15 -0400

    Update intialize.js

--- Project/src/intialize.js
+++ Project/src/intialize.js
@@ -1,3 +1,6 @@
+
+// Set dimensions
+
 const HEIGHT  = 800;
 const WIDTH   = 1200;
 const PADDING = 100;
@@ -6,7 +9,7 @@ const MID_HEIGHT = HEIGHT / 2;
 const MID_WIDTH  = WIDTH / 2;
 
 const SPRITE_SIZE = 10;
-const BOSS_SIZE   = 40;
+const BOSS_SIZE   = 30;
 
 const MIN_OBSTACLES = 2;
 const MAX_OBSTACLES = 6;
@@ -19,12 +22,30 @@ const HEALTH_HEIGHT = 50;
 
 const BORDER = 5;
 
+const ENEMY_BONUS = 25;
+const BOSS_BONUS  = 50;
+
+const MINES_COUNT = 3;
+
 
 let BULLET_SPEED  = 4;
 let PLAYER_SPEED  = 4;
 let ENEMY_SPEED   = 3.5;
 let MAX_HEALTH    = 4;
 let MAX_SHOTS     = 5;
+
+let wave_id = 0;
+
+// Powerups
+
+let freeze_duration = 0;
+let slowing_factor  = 1;
+let mines_left      = 0;
+
+const POWER_NAMES         = ["freeze", "mine", "laser"]; 
+const BUTTON_POSITIONS    = [220, 320, 420];
+const BASE_POWERUP_SCORES = [300, 800, 1500];
+
 
 const ROUNDS  = 20;
 
@@ -34,10 +55,10 @@ const COLOURS = [0x75D5FD, 0x09FBD3, 0xFE53BB, 0xFFDEF3, 0xFF2281, 0x13CA91,  //
 const WHITE = 0xFFFFFF;
 const BLACK = 0x000000;
 
-const ENEMY_TYPES = ["triangle", "square", "pentagon", "hexagon"];
+const ENEMY_TYPES = ["triangle", "square", "pentagon", "hexagon", "octagon"];
 const COLOUR_NAMES = ["black", "yellow", "green", "blue", "purple", "red", "orange"];
 
-const STARTING_ROUNDS = [10, 0, 20, 40];
+const STARTING_ROUNDS = [9, 0, 19, 39];
 
 
 function rand_range(low, high){
