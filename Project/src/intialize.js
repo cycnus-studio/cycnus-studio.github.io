a1a3b0b31cb2e5060df9eef1cf969551624497d8
cycnus-studio <50322929+cycnus-studio@users.noreply.github.com>
2019-06-14 07:35:39 -0400

    Update intialize.js

--- Project/src/intialize.js
+++ Project/src/intialize.js
@@ -35,6 +35,7 @@ let MAX_HEALTH    = 4;
 let MAX_SHOTS     = 5;
 
 let wave_id = 0;
+let on_pause = true;
 
 // Powerups
 
@@ -52,6 +53,8 @@ const ROUNDS  = 20;
 const COLOURS = [0x75D5FD, 0x09FBD3, 0xFE53BB, 0xFFDEF3, 0xFF2281, 0x13CA91,  // Player colours
 				 0xFFFFFF, 0xFFE66D, 0xBCE784, 0xA4FFEE, 0x915E87, 0xBC1E1E, 0xF4A261]; // Enemy colours
 
+const BLUE_COLOURS = [0x50c0ed, 0x98d7e2, 0x2da9ff, 0x0d1dc6, 0x2dfff8, 0xc2f2f9, 0x7dc2ed];
+
 const WHITE = 0xFFFFFF;
 const BLACK = 0x000000;
 
@@ -60,10 +63,35 @@ const COLOUR_NAMES = ["black", "yellow", "green", "blue", "purple", "red", "oran
 
 const STARTING_ROUNDS = [9, 0, 19, 39];
 
+let enemies   = [];
+let bullets   = [];
+let obstacles = [];
+let mines     = [];
+let particles = [];
 
-function rand_range(low, high){
+let blue_colour_bg = false;
+
+/*                                      */
+
+let type = "WebGL";
+const renderer = PIXI.autoDetectRenderer();
+
+if(!PIXI.utils.isWebGLSupported())
+    type = "canvas";
+
+PIXI.utils.sayHello(type);
+
+
+let particle_container = new PIXI.Container();
+let background_particle_container = new PIXI.Container();
+
+
+function rand_range(low, high, is_float = false){
 
 	let range = high - low + 1;
+	
+	if(is_float == true)
+		return Math.random() * range + low;
 
 	return Math.floor(Math.random() * range + low);
 
